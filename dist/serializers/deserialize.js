79d6b580b429e50a3cdd8c50c91a4ebb66c45db1
Andrei Sidorov <andretges@gmail.com>
2017-11-27 19:00:09 +0300

    Optimize deserialization

--- dist/serializers/deserialize.js
+++ dist/serializers/deserialize.js
@@ -4,17 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
-
-var _defineProperty3 = _interopRequireDefault(_defineProperty2);
-
 var _keys = require('babel-runtime/core-js/object/keys');
 
 var _keys2 = _interopRequireDefault(_keys);
 
-var _extends4 = require('babel-runtime/helpers/extends');
+var _extends2 = require('babel-runtime/helpers/extends');
 
-var _extends5 = _interopRequireDefault(_extends4);
+var _extends3 = _interopRequireDefault(_extends2);
 
 var _humps = require('humps');
 
@@ -26,8 +22,6 @@ function deserializeRelationships() {
 
   return resources.map(function (resource) {
     return deserializeRelationship(resource, store);
-  }).filter(function (resource) {
-    return !!resource;
   });
 }
 
@@ -35,11 +29,14 @@ function deserializeRelationship() {
   var resource = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
   var store = arguments[1];
 
-  if (resource && store[(0, _humps.camelize)(resource.type)] && store[(0, _humps.camelize)(resource.type)][resource.id]) {
-    return deserialize((0, _extends5.default)({}, store[(0, _humps.camelize)(resource.type)][resource.id], { meta: { loaded: true } }), store);
+  if (resource) {
+    var resourceType = (0, _humps.camelize)(resource.type);
+    if (store[resourceType] && store[resourceType][resource.id]) {
+      return deserialize((0, _extends3.default)({}, store[resourceType][resource.id], { meta: { loaded: true } }), store);
+    }
   }
 
-  return deserialize((0, _extends5.default)({}, resource, { meta: { loaded: false } }), store);
+  return deserialize((0, _extends3.default)({}, resource, { meta: { loaded: false } }), store);
 }
 
 function deserialize(_ref, store) {
@@ -51,23 +48,26 @@ function deserialize(_ref, store) {
 
   var resource = { _type: type, _meta: meta };
 
-  if (id) resource = (0, _extends5.default)({}, resource, { id: id });
+  if (id) resource['id'] = id;
 
   if (attributes) {
     resource = (0, _keys2.default)(attributes).reduce(function (resource, key) {
-      return (0, _extends5.default)({}, resource, (0, _defineProperty3.default)({}, (0, _humps.camelize)(key), attributes[key]));
+      resource[(0, _humps.camelize)(key)] = attributes[key];
+      return resource;
     }, resource);
   }
 
   if (relationships) {
     resource = (0, _keys2.default)(relationships).reduce(function (resource, key) {
-      return (0, _extends5.default)({}, resource, (0, _defineProperty3.default)({}, (0, _humps.camelize)(key), function () {
-        if (Array.isArray(relationships[key].data)) {
-          return deserializeRelationships(relationships[key].data, store);
+      var relatedResource = relationships[key].data;
+      resource[(0, _humps.camelize)(key)] = function () {
+        if (Array.isArray(relatedResource)) {
+          return deserializeRelationships(relatedResource, store);
         } else {
-          return deserializeRelationship(relationships[key].data, store);
+          return deserializeRelationship(relatedResource, store);
         }
-      }));
+      };
+      return resource;
     }, resource);
   }
 
